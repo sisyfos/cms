f4b742c2bab4746e26c97221dd64fa41bab01dc4
Christopher Holmberg <christopherholmberg@live.se>
2013-04-17 12:16:29 +0200

    Åt helvete med projektet.

--- CMS/Controllers/HomeController.cs
+++ CMS/Controllers/HomeController.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using CMS.Controllers;
+using System.IO;
+using System.Web.Helpers;
+using CMS.Models;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 namespace CMS.Controllers
 {
     public class HomeController : Controller
@@ -20,6 +29,32 @@ namespace CMS.Controllers
             ViewBag.Message = "Your app description page.";
 
             return View();
+        }
+
+        public ActionResult Imagenew()
+        {
+            var model = new ImageModel()
+            {
+                Images = Directory.EnumerateFiles(Server.MapPath("~/images_upload/"))
+                                  .Select(fn => "~/images_upload/" + Path.GetFileName(fn))
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Imagenew(ImageModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string fileName = Guid.NewGuid().ToString();
+                string serverPath = Server.MapPath("~");
+                string imagesPath = serverPath + "/images_upload";
+                string thumbPath = imagesPath + "/images_upload";
+                string fullPath = imagesPath + "/images_upload";
+                ImageModel.ResizeAndSave(thumbPath, fileName, model.ImageUploaded.InputStream, 80, true);
+                ImageModel.ResizeAndSave(fullPath, fileName, model.ImageUploaded.InputStream, 600, true);
+            }
+            return RedirectToAction("Imagenew");
         }
 
         public ActionResult Contact()
